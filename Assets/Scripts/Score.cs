b42ff6f219290271616a1d2c33614f107df85169
Sebastián_Barrón <ing.sebastianbarron@gmail.com>
2022-11-06 14:16:22 -0600

    Lo de Angel >:v

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+	// Use this for initialization
+	public Text txt;
+	public int score;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		txt.text = "" + score;
+	}
+
+	public void updateScore(int deltaScore)
+	{
+		score += deltaScore;	
+	}
+}
